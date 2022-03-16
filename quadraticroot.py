0c50131e4afb5d858f4d941823fca06c957fae49
Ajaygautham2110 <101620150+Ajaygautham2110@users.noreply.github.com>
2022-03-16 10:45:39 +0530

    Add files via upload

---
+++ quadraticroot.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+num = int(input("enter the number : "))
+sqroot = num**0.5
+
+print('square root of',num,'is',sqroot)
